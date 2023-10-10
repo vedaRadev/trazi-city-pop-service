611fbbe3510680b324f3a91365fbd8261eac796e
vedaRadev <ryancarmstrong97@gmail.com>
2023-10-10 15:58:58 -0400

    when generating database, create table with primary key

--- generate-database.js
+++ generate-database.js
@@ -40,7 +40,13 @@ const insertionValuesString = records.reduce((acc, [ city, state, population ]) 
 }, '');
 
 db.serialize(() => {
-  db.run(`CREATE TABLE city_populations (city TEXT NOT NULL, state TEXT NOT NULL, population INTEGER NOT NULL);`);
+  db.run(`
+    CREATE TABLE city_populations (
+      city TEXT NOT NULL,
+      state TEXT NOT NULL,
+      population INTEGER NOT NULL,
+      PRIMARY KEY (state, city)
+    );`);
   db.run(`INSERT INTO city_populations (city,state,population) VALUES ${insertionValuesString};`);
 });
 
